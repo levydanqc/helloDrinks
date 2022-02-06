556bc80b03a416d1ec68fe4e1a2797e91ff086ea
Dan Levy <danlevy.ca@icloud.com>
2022-02-05 23:51:28 -0500

    Création d'un formulaire d'usager.

--- helloDrinks/forms.py
+++ helloDrinks/forms.py
@@ -5,7 +5,18 @@ from .models import Usager
 class UsagerForm(forms.ModelForm):
     class Meta:
         model = Usager
-        fields = ['email', 'nom', 'prenom', 'pseudo', 'password',
-                  'adresse', 'ddn', 'alcoolPref']
-        labels = {'ddn': 'Date de naissance'}
-        help_texts = {'ddn': 'Veuillez entrer votre date de naissance'}
+        # fields = ['email', 'nom', 'prenom', 'pseudo', 'password',
+        #           'adresse', 'ddn', 'alcoolPref']
+        labels = {'ddn': 'Date de naissance', "alcoolPref": "Alcool préféré"}
+        # help_texts = {'ddn': 'Veuillez entrer votre date de naissance'}
+        fields = "__all__"
+        widgets = {
+            'nom': forms.TextInput(attrs={'class': 'form-control my-2'}),
+            'prenom': forms.TextInput(attrs={'class': 'form-control my-2'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control my-2'}),
+            'pseudo': forms.TextInput(attrs={'class': 'form-control my-2'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control my-2'}),
+            'adresse': forms.TextInput(attrs={'class': 'form-control my-2'}),
+            'ddn': forms.DateInput(attrs={'class': 'form-control my-2', 'type': 'date'}),
+            'alcoolPref': forms.Select(attrs={'class': 'form-control my-2'}),
+        }
--- helloDrinks/models.py
+++ helloDrinks/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Usager(models.Model):
     def __str__(self) -> str:
         return self.pseudo
     nom = models.CharField(max_length=50)
     prenom = models.CharField(max_length=50)
-    adresse = models.CharField(max_length=100)
+    pseudo = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=50)
-    ddn = models.DateField()
-    pseudo = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=100)
+    ddn = models.DateField()
     alcoolPref = models.ForeignKey('Alcool', on_delete=models.CASCADE)
 
 

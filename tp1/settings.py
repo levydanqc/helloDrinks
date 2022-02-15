8ea015c1d8b7b7c0cf34e0cc65c9476ae84c8eea
Dan Levy <danlevy.ca@icloud.com>
2022-02-15 19:35:44 +0000

    Configuration du déployement sur serveur.

--- tp1/settings.py
+++ tp1/settings.py
@@ -16,17 +16,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "(G-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-JaNdRfUjXn2r5u8x/"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
 ]
--- tp1/wsgi.py
+++ tp1/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/hellodrinks/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tp1.settings')
 
 application = get_wsgi_application()
